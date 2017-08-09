f7628c80b51fb90571a2130858eee2f23f839696
Jonathan Nacionales <jnaciona@gmail.com>
2017-08-09 10:13:14 -0700

    Remove unneeded dependencies
    The previous version of ecommerce-product had an unneccesary
    dependency on React Native. I've changed it so that the module
    uses react-primitive properly

--- packages/components/ecommerce-product/src/index.js
+++ packages/components/ecommerce-product/src/index.js
@@ -1,36 +1,10 @@
 import React from "react"
-const ReactPrimitives = require('react-primitives');
-const {
-  Animated,
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  Platform,
-  Easing,
-  Dimensions,
-} = require('react-native');
-
-ReactPrimitives.inject({
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  Easing,
-  Animated,
-  Platform: {
-    OS: Platform.OS,
-    Version: Platform.Version,
-  },
-  Dimensions
-});
-//import { View, Text, Image, StyleSheet } from 'react-primitives';
+import { View, Text, Image, StyleSheet } from 'react-primitives';
 
 export default class Product extends React.Component {
   render() {
     return (
       <View style={styles.product}>
-        <Image></Image>
         <Text>Title</Text>
         <Text>By Line</Text>
         <Text>Actual Price</Text>
