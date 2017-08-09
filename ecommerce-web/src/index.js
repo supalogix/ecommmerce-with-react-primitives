6de84af04fd9762cd1306a693efd14a531b02961
Jonathan Nacionales <jnaciona@gmail.com>
2017-08-08 22:34:33 -0700

    Rename product package to ecommerce-product

--- ecommerce-web/src/index.js
+++ ecommerce-web/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {render} from "react-dom"
 import { View, Text, Image, StyleSheet } from 'react-primitives';
-import Product from "product"
+import Product from "ecommerce-product"
 
 render(
     <Product />,
--- packages/components/ecommerce-product/src/index.js
+++ packages/components/ecommerce-product/src/index.js
@@ -1,5 +1,30 @@
 import React from "react"
-import { View, Text, Image, StyleSheet } from 'react-primitives';
+const ReactPrimitives = require('react-primitives');
+const {
+  Animated,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Platform,
+  Easing,
+  Dimensions,
+} = require('react-native');
+
+ReactPrimitives.inject({
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Easing,
+  Animated,
+  Platform: {
+    OS: Platform.OS,
+    Version: Platform.Version,
+  },
+  Dimensions
+});
+//import { View, Text, Image, StyleSheet } from 'react-primitives';
 
 export default class Product extends React.Component {
   render() {
