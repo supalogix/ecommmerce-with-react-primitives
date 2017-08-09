3751792f61c6f7a7733bf8f970a3904a53e3bb29
Jonathan Nacionales <jnaciona@gmail.com>
2017-08-08 22:38:49 -0700

    Add react-native support

---
+++ ecommerce-native/index.ios.js
@@ -0,0 +1,6 @@
+import ecommerceNative from "./ios_src"
+import { AppRegistry } from "react-native"
+
+export default ecommerceNative
+
+AppRegistry.registerComponent('ecommerceNative', () => ecommerceNative);
